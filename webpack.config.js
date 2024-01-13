de78a8b853ba772762722c84389cc361adac89cb
wwwchendev <wwwchendev@gmail.com>
2024-01-13 21:16:12 +0800

    載入babel

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,18 @@ module.exports = () => {
           type: 'asset/resource',
         },
         {
+          test: /\.(?:js|mjs|cjs)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
+            }
+          }
+        },
+        {
           test: /\.css$/,
           use: [
             Mode == 'development'
