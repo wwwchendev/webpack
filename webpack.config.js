a3c6eb91f14ff4b66cddd0184c16c8af77252e69
wwwchendev <wwwchendev@gmail.com>
2024-01-13 21:05:16 +0800

    1. 安裝HTML&CSS模組套件 2. 配置webpack.config.js+使用alias設定路徑縮寫

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // 載入模組
 const path = require('path')
 //console.log(__dirname); //>> C:\Users\..\Desktop\Test
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // 設定
 const Mode = process.env.NODE_ENV === 'production' ? 'production' : 'development' //透過 cross-env搭配scripts  process.env.NODE_ENV環境變數的過程
@@ -15,7 +17,23 @@ const Config = {
     filename: 'js/[name].js', // 輸出JS的檔案名稱
     clean: true, // 啟用自動清理輸出目錄
     assetModuleFilename: 'assets/[name][ext]',
-  }
+  },
+  //- Webpack別名配置
+  alias: {
+    '@assets': path.resolve(__dirname, './src/assets/'),
+    '@img': path.resolve(__dirname, './src/assets/images'),
+    '@css': path.resolve(__dirname, './src/css'),
+  },
+  //- HTML
+  pages: {
+    index: {
+      template: './src/html/index.html', // 來源HTML文件
+      chunks: ['index'], // 注入的JavaScript chunks，這裡使用了 'index'，對應到 entries 中的 'index'
+      filename: 'html/index.html', // 輸出的HTML檔案名稱
+    },
+  },
+  //- CSS
+  cssFileName: { filename: 'css/[name].css' },
 }
 
 module.exports = () => {
@@ -26,5 +44,43 @@ module.exports = () => {
     mode: Mode || 'development',
     entry: Config.entries,
     output: Config.outputs,
+    resolve: { alias: Config.alias },
+    module: {
+      rules: [
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.css$/,
+          use: [
+            Mode == 'development'
+              ? 'style-loader'
+              : MiniCssExtractPlugin.loader,
+            //如果你在設定中使用了MiniCssExtractPlugin.loader，就表示你想要將CSS提取到獨立的檔案中，而不需要將樣式內聯到JavaScript檔案中，因此不再需要style-loader。
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin(Config.pages.index),
+      Mode == 'production' && new MiniCssExtractPlugin(Config.cssFileName),
+    ].filter(Boolean), // 過濾掉空的插件,
+    // devServer: {
+    //   static: [
+    //     {
+    //       directory: path.join(__dirname, 'dist/'),
+    //       watch: true,
+    //     },
+    //   ],
+    //   open: true, //自動在預設瀏覽器中開啟應用程式
+    //   hot: true, //啟用熱模塊
+    //   port: 8080, //server指令監聽port
+    //   devMiddleware: {
+    //     index: 'html/index.html', // 指定要打開的預設文件
+    //   },
+    // },
+    // devtool: Mode === 'production' ? 'inline-source-map' : 'source-map', //啟用映射路徑 (Source Map)
   }
 }
